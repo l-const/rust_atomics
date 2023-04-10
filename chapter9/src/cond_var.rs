99ce4173439943c119d4871f4974f1756c3c4c18
l-const <const_l@outlook.com>
2023-04-11 00:27:44 +0300

    cond var & rwlock

--- chapter9/src/cond_var.rs
+++ chapter9/src/cond_var.rs
@@ -12,6 +12,7 @@ pub struct CondVar {
 
 
 impl CondVar {
+
     pub const fn new() -> Self {
         Self {
             counter: AtomicU32::new(0),
@@ -20,6 +21,7 @@ impl CondVar {
     }
 
     pub fn wait<'a, T>(&self, guard: MutexGuard<'a, T>) -> MutexGuard<'a, T> {
+        self.num_waiters.fetch_add(1, Relaxed);
         let counter_value = self.counter.load(Relaxed);
 
         // Unlock the mutex so we can lock it again later.
@@ -30,16 +32,60 @@ impl CondVar {
         // Wait, but only if the counter hasn't changed since unlocking.
         wait(&self.counter, counter_value);
 
+        self.num_waiters.fetch_sub(1, Relaxed);
+
         mutex.lock()
     }
 
     pub fn notify_one(&self) {
-        self.counter.fetch_add(1, Relaxed);
-        wake_one(&self.counter);
+        if self.num_waiters.load(Relaxed) > 0 {
+            self.counter.fetch_add(1, Relaxed);
+            wake_one(&self.counter);
+        }
     }
 
     pub fn notify_all(& self) {
-        self.counter.fetch_add(1, Relaxed);
-        wake_all(&self.counter);
+        if self.num_waiters.load(Relaxed) > 0 {
+            self.counter.fetch_add(1, Relaxed);
+            wake_all(&self.counter);
+        }
     }
+}
+
+
+mod tests {
+    use std::thread;
+    use super::CondVar;
+
+
+    #[test]
+    fn test_condvar() {
+        let mutex = crate::mutex_content::Mutex::new(0);
+        let condvar = CondVar::new();
+
+        let mut wakeups = 0;
+
+        thread::scope(|s| {
+            s.spawn(|| {
+                thread::sleep(std::time::Duration::from_secs(1));
+                *mutex.lock() = 123;
+                condvar.notify_one();
+            });
+
+
+            let mut m  = mutex.lock();
+            while *m < 100 {
+                m = condvar.wait(m);
+                wakeups += 1;
+            }
+
+            assert_eq!(*m, 123);
+        });
+
+    // Chceck that the main thread actually did wait (not busy-loop),
+    // while still allowing for a few spurious wak ups.
+    assert!(wakeups < 10);
+    assert!(wakeups == 1);
+    }
+
 }
