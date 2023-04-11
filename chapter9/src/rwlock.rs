543ebe906f54c4633d580f7faf9723e2e092be14
Kostas Lampropoulos <kostas.lampropoulos@openbet.com>
2023-04-11 14:11:55 +0300

    finished chapter10

--- chapter9/src/rwlock.rs
+++ chapter9/src/rwlock.rs
@@ -1,4 +1,4 @@
-use std::{cell::UnsafeCell, sync::atomic::AtomicU32, ops::DerefMut};
+use std::{cell::UnsafeCell, sync::atomic::AtomicU32, ops::{Deref, DerefMut}};
 use std::sync::atomic::Ordering::*;
 
 use atomic_wait::{wait, wake_one, wake_all};
@@ -6,6 +6,7 @@ use atomic_wait::{wait, wake_one, wake_all};
 pub struct RwLock<T> {
     /// The number of readers, or u32::MAX if wite-locked.
     state: AtomicU32,
+    writer_wake_counter: AtomicU32,
     value: UnsafeCell<T>,
 }
 
@@ -16,12 +17,13 @@ impl <T> RwLock<T> {
     pub const fn new(value: T) -> Self {
         Self {
             state: AtomicU32::new(0), // Unlocked
+            writer_wake_counter: AtomicU32::new(0),
             value: UnsafeCell::new(value),
         }
     }
 
     pub fn read(&self) -> ReadGuard<T> {
-        let mut s = self.state.load(std::sync::atomic::Ordering::Relaxed);
+        let mut s = self.state.load(Relaxed);
         loop {
             if s < u32::MAX {
                 assert!(s != u32::MAX - 1, "too many readers");
@@ -37,14 +39,17 @@ impl <T> RwLock<T> {
         }
     }
 
-
     pub fn write(&self) -> WriteGuard<T> {
-        while let Err(s) = self.state.compare_exchange(0, u32::MAX, Acquire, Relaxed) {
-            // Wait while already locked.
-            wait(&self.state, s);
+        while self.state.compare_exchange(0, u32::MAX, Acquire, Relaxed).is_err() {
+            let w = self.writer_wake_counter.load(Acquire);
+            if self.state.load(Relaxed) != 0 {
+                // Wait while already locked.
+                wait(&self.writer_wake_counter, w);
+            }
         }
         WriteGuard { rwlock: self }
     }
+
 }
 
 
@@ -55,7 +60,8 @@ pub struct ReadGuard<'a, T> {
 impl <T> Drop for ReadGuard<'_, T> {
     fn drop(&mut self) {
         if self.rwlock.state.fetch_sub(1, Release) == 1 {
-            wake_one(&self.rwlock.state);
+            self.rwlock.writer_wake_counter.fetch_add(1, Release);
+            wake_one(&self.rwlock.writer_wake_counter);
         }
     }
 }
@@ -79,7 +85,7 @@ impl <T> Drop for WriteGuard<'_, T> {
     }
 }
 
-impl<T>  std::ops::Deref for WriteGuard<'_, T> {
+impl<T>  Deref for WriteGuard<'_, T> {
     type Target = T;
     fn deref(&self) -> &T {
         unsafe { &*self.rwlock.value.get()}
